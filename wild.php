811939b9e6086b6e32fe980e507ca01ccc182b5d
David Pellecuer <pellecuer.david@gmail.com>
2018-03-23 17:33:16 +0100

    refonte du code html

---
+++ wild.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+    <head>
+      <meta charset="utf-8" >
+      <title>Mon Formulaire html</title>
+
+      <link rel="stylesheet" href="styleFormulaire.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+
+    <body>
+    </body>
+</html>
